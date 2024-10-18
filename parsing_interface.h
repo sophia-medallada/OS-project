41d533387704fbbcd8501c4cc3d3ed18ed1a4be3
Lisa Phan <157837595+LisaPhan3010@users.noreply.github.com>
2024-10-18 13:31:28 -0500

    Update parsing_interface.h
    Update the header file after debugging

--- parsing_interface.h
+++ parsing_interface.h
@@ -3,7 +3,7 @@
 #include <stdbool.h>
 
 typedef struct {
-    char *args; //define array of arguments
+    char **args; //define array of arguments
     char *infile; //input redirection
     char *outfile; //output redirection
     bool append; //flag indicating
@@ -13,7 +13,7 @@ typedef struct {
 //Function prototypes
 ParsedCommand* parseCmd(const char* input); //parse command input
 void free_parsedCmd(ParsedCommand* command); //free parse command
-void quitCmd(const ParsedCommand* command); //check quit or exit command
+bool quitCmd(const ParsedCommand* command); //check quit or exit command
 
 
 #endif
