794b35348c0ad226189ac76f24fa229420fe8a15
Lisa Phan <157837595+LisaPhan3010@users.noreply.github.com>
2024-10-18 13:36:51 -0500

    Implement execute.h for execute

--- execute.h
+++ execute.h
@@ -1,1 +1,10 @@
+#ifndef EXECUTE_H
+#define EXECUTE_H
+#include <stdbool.h>
+#include "parsing_interface.h"
 
+void execCmd(const ParsedCommand* command); //executes a parsed command
+void execBuiltin(const ParsedCommand* command); //executes a built-in command
+bool backgroundJob(const ParsedCommand* command); //check background job command
+
+#endif
