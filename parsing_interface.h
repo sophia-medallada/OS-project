a6afaf81d91e2035a796f5ffc4565cafe79e94fe
Lisa Phan <157837595+LisaPhan3010@users.noreply.github.com>
2024-10-18 12:53:52 -0500

    Update parsing_interface.h
    implement a header file for parsing interface

--- parsing_interface.h
+++ parsing_interface.h
@@ -1,1 +1,19 @@
+#ifndef PARSING_INTERFACE_H
+#define PARSING_INTERFACE_H
+#include <stdbool.h>
 
+typedef struct {
+    char *args; //define array of arguments
+    char *infile; //input redirection
+    char *outfile; //output redirection
+    bool append; //flag indicating
+    bool background; //flag indicating
+} ParsedCommand;
+
+//Function prototypes
+ParsedCommand* parseCmd(const char* input); //parse command input
+void free_parsedCmd(ParsedCommand* command); //free parse command
+void quitCmd(const ParsedCommand* command); //check quit or exit command
+
+
+#endif
