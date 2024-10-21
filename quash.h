c2e510b7c14a35bd4202febe033ca661b3c0deb3
Lisa Phan <157837595+LisaPhan3010@users.noreply.github.com>
2024-10-21 00:57:39 -0500

    Update quash.h

---
+++ quash.h
@@ -0,0 +1,11 @@
+#ifndef QUASH_H
+#define QUASH_H
+
+#include "execute.h"
+#include "parsing_interface.h"
+
+void initQuash(); //define quash
+void Quash(); //main function to run the shell
+void quashCleanup(); //function of cleaning before exiting the quash
+
+#endif
