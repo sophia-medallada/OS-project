f394450e80ef005591fbc8369546a877e3bf91de
Lisa Phan <157837595+LisaPhan3010@users.noreply.github.com>
2024-10-21 00:58:07 -0500

    Update quash.c

---
+++ quash.c
@@ -0,0 +1,42 @@
+
+#include "quash.h"
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+void initQuash() { //define quash
+    printf("Welcome to Quash!\n"); 
+}
+
+void Quash() { //main function of Quash
+     char input[1024]; //buffer for holding the input command
+     while (1) { //create loop for the running shell
+         printf("[QUASH]$ "); //print the prompt
+        if (fgets(input, sizeof(input), stdin) == NULL) { //in case the input is empty or there is an error while reading input
+            //perror("fgets"); //print error message
+            break; //then break the loop
+        }
+        ParsedCommand command; //parse the input command
+        if (parseCmd(input, &command)) {
+            execCmd(&command); //execute the command if parsing was successful
+            continue;
+        }
+
+        free_parsedCmd(&command); //free the command after executing
+
+     }
+     return;
+}
+
+void quashCleanup() { //free all resources befor exiting
+    printf("Exiting Quash...\n");
+}
+
+int main() {
+    initQuash();
+    Quash();
+    quashCleanup();
+    return 0;
+}
