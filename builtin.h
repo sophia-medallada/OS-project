fac03324053a4eeebbcb1ecce01c473289675d7d
Lisa Phan <157837595+LisaPhan3010@users.noreply.github.com>
2024-10-22 15:20:35 -0500

    Update builtin.h

--- builtin.h
+++ builtin.h
@@ -3,12 +3,20 @@
 
 #include "parsing_interface.h"
 #include <stdbool.h>
+#include <unistd.h> //POSIX operating system API
+
+typedef struct {
+    pid_t pid;
+    char command[1024];
+    int status; 
+} BackgroundJob;
 
 bool cd(const ParsedCommand* command); //change directory command
 bool pwd(const ParsedCommand* command); //check current working directory
 bool echo(const ParsedCommand* command); //print stdout
 bool export(const ParsedCommand* command); //set up enviroment variables
 bool jobsCmd(const ParsedCommand* command); //list of background jobs
+bool quitCmd(const ParsedCommand* command); //quit or exit quash
 
 //check if a command is a builtin command
 bool isBuiltin(const ParsedCommand* command);
