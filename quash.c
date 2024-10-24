8970b8504e6a8b890940e90c8281d8db9a2100e5
Sophia Medallada <154625869+sophia-medallada@users.noreply.github.com>
2024-10-24 01:00:21 -0500

    Update quash.c

--- quash.c
+++ quash.c
@@ -10,20 +10,43 @@ void initQuash() { //define quash
     printf("Welcome to Quash!\n"); 
 }
 
+
 void Quash() { //main function of Quash
      char input[1024]; //buffer for holding the input command
+     
+     //Sophia's change 
+     struct sigaction sa;
+     sa.sa_handler = &sigchild_handler;
+     sa.sa_flags = SA_RESTART | SA_NOCLDSTOP;
+     sigemptyset(&sa.sa_mask);
+     if (sigaction(SIGCHLD, &sa, NULL) == -1) {
+        perror("sigaction");
+        exit(EXIT_FAILURE);
+     }
+
      while (1) { //create loop for the running shell
          printf("[QUASH]$ "); //print the prompt
         if (fgets(input, sizeof(input), stdin) == NULL) { //in case the input is empty or there is an error while reading input
             perror("fgets"); //print error message
             break; //then break the loop
         }
+        input[strcspn(input, "\n")] =0;
+
+        if (strlen(input) == 0) {
+            continue;
+        }
         ParsedCommand command; //parse the input command
         if (parseCmd(input, &command)) {
             execCmd(&command); //execute the command if parsing was successful
             continue;
         }
-
+        if (isBuiltin(&command)) {
+            if (!execBuiltin(&command)) {
+                fprintf(stderr, "Failed to execute built-in command\n");
+            }
+        } else {
+            execCmd(&command);
+        }
         free_parsedCmd(&command); //free the command after executing
 
      }
@@ -33,6 +56,9 @@ void Quash() { //main function of Quash
 void quashCleanup() { //free all resources befor exiting
     printf("Exit...\n");
 }
+
+
+
 
 int main() {
     initQuash();
